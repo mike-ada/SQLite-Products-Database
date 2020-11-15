73e7834c44872e36876e5a0dee255cdffeed653d
Mike <55744820+mike-ada@users.noreply.github.com>
2020-11-15 19:10:59 +0000

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,5 +1,8 @@
+-- sqlite documentation: https://sqlite.org/docs.html
+
 .header on --set the flag to display field heading
 .mode column --set the flag to enable tabular output with one row per line
 .timer on --set the flag to show the execution time
 
 .open "products_sqlite.db" --open the products database
+
