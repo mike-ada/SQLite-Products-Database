fe41a986fd5eba2a4b194f13b19ee1086f45d59c
Mike <55744820+mike-ada@users.noreply.github.com>
2020-11-15 19:09:24 +0000

    Create main.sql

---
+++ main.sql
@@ -0,0 +1,5 @@
+.header on --set the flag to display field heading
+.mode column --set the flag to enable tabular output with one row per line
+.timer on --set the flag to show the execution time
+
+.open "products_sqlite.db" --open the products database
